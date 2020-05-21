3f4ae2df0e2e4c22139acb01849f6fe474499eeb
David Everett <davideverett1989@gmail.com>
2020-05-20 21:04:47 -0500

    Auth is working.

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import './App.scss';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import NavBar from '../components/NavBar/NavBar';
 import Auth from '../components/Auth/Auth';
 import Team from '../components/Team/Team';
 
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <Team />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
       <div className="App text-white">
-        <NavBar />
-        <Auth />
-        <Team />
+        <NavBar authed={authed}/>
+        {loadComponent()}
       </div>
     );
   }
